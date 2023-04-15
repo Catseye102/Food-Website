c1a7ebbcdd0aed002d16e2463ecdfaf3dfbb0057
Catseye102 <hasnat.jamil99@gmail.com>
2023-04-16 04:28:09 +0600

    Initial upload

---
+++ home.php
@@ -0,0 +1,46 @@
+<html lang="en">
+  <head>
+      <meta charset="utf-8"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <meta name="description" content="About the site"/>
+      <meta name="author" content="Author name"/>
+      <title> Login </title>
+    
+      <!-- core CSS -->
+      <link href="css/bootstrap.min.css" rel="stylesheet"/>
+      <link href="css/font-awesome.min.css" rel="stylesheet"/>
+      <link href="css/animate.min.css" rel="stylesheet"/>
+      <link href="css/main.css" rel="stylesheet"/> 
+  <script type="text/javascript" src="https://gc.kis.v2.scr.kaspersky-labs.com/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js?attr=mQViQsKnPh7A5v_cRIyaPhFfTyjInv04RVoO1fhs-ukELaqEhnucmLhBrT_Whu5A9NX86iuSzsxOpA1BxMVgjkRBX566NP-EZXOfTvJZ28aySM-uDNmdktWCR39jcYMGmEusZxmXVXynyjW4ZDLBw-5sHhBo1VnQ6tRvUexAE6H0QlsEJ4g2mJGsVzD98s-9vGhOWu5SkJGDhr7v-m7CNAyct5kM0dinAeKrITz50FFtiSfiGPfgf-jrxcF4Q_0cJVfdmzPe-0XQ52uoYYtpZK5aVKt8w3AOb6NlO2CEJdhI2uSUG5FHouOGTiVu7Y7ydi1XWRUwJrVboXmeWLffBkHjMvMCRWX1l4pMThPd_bfx8WS5L68pouN0hMgMuj_LkTYAva9YvHdFvuMcJ_yHr4H4-3gao28QuFrHOi_OnwZLxnpNcWH8BaK136uI9wlF0JPi41FFkKD2RQ03BblIVXegtLwYjYCmEoWVrQSssMO91ITflAbQnBf2BHK0V8l92L2GbWVfHvKHfEhIT-iE9aq4mdY1gM3dht9eN4n87NPX2Cx9WkcVUrqaZTJDHNnwcH_k_Mn5kTEPM99CVReZfv98OB6jk5KUWbGxBm1VBrYBdfzfJ-Uga-tyYxvxgyLe6QsediOfP4FEWRSFl7H5-8La49VVcHVBeGLyVA0Sq0-hwKeQKmRcOCl0XZmCR-rBEboiDXWnXnZpwM289hDDzHQDX4KUubtS2WZPMAmZiHYkyguwywGkDiGFIt6l7NT193teDpm1zM8mSRhcxZuLXiSCVB97yo_XAX4erwSDOCuI2VIA5RiMwZ78dRqBqyYW83VnTyoxIfkSVifLtPVVvQSEViretzEAp-YeIizg5_evzI3xSmYE7K7qhUBGT1tcf-UOT6A4yQwjIY94NaVoi2DcdNjEE-rlUbO5DhsugITZJa7S3x-N5xmBdlSmS8mgOjcK7PP64HKar8ngFMVVxlWv9BQHN502OuRLPbTUXHDXEW4A6Y9-O2gIYs170K70Xj80jMvzYc6GQ9x9PviKYn8AY5eTGxEACHMmNlMl_uuQ3DiAI9tot-PKlRYaQvV7ryo08gnhx_GYHfUNpUNBy06mLwgXNE4vfZWF3nifxwXvbo9d6cOWc0jOs6wz5C68eG3Wi1rmh_ZMpdaVtjKztggy79v3vvMJGmy3oZ8uHQUTu2LIkZhKMwBdXB9m8s9p_X4yuVbs8M2sIs7ql56wZt5GGls_hDWSjnMtv1FncWXGFD5IlcvzpC9lJNoghnUJ6KuGP3Y692gR5TYlogQkCBb2VrDxNUMbDPNvhNpKav5fXLXpTqljcpptELl1cJo_V7S-JIpVlwvi_IfmEgMQnRmg9YHjify0_plEbaeWLiIi7Tq0YvHUSiuaAf9KQsieKQpgJTUPKWxhfEQfECNyuICF-8obWo6_nRM_mmurg49TfOsDIEV5pjDyvv-e8V0_MUZCfoTM3t5SQiNvP8KVd5B1FWY8ILFs51sYxyRQOCz1Iyb1-wxGldRci5Zw_6Oe46Sb7jWYweULHZHbFocFI30e-DEkJRdBNGbuK8yHDCIK603rc08q5baz2R3gFzEruaDGN-RAV02qTCO177Z8-4lWcMRff3rqSWwCI1FYSaxkrhCbCueLfLOQj74OpjbL" charset="UTF-8"></script><link rel="stylesheet" crossorigin="anonymous" href="https://gc.kis.v2.scr.kaspersky-labs.com/E3E8934C-235A-4B0E-825A-35A08381A191/abn/main.css?attr=aHR0cHM6Ly9kb2MtMHMtODgtZG9jcy5nb29nbGV1c2VyY29udGVudC5jb20vZG9jcy9zZWN1cmVzYy81djRhcXQ4bGE2aGlqYzN2NmU5ZW5nc2M3YjVxbTA0bS8zNW9nb29zbXRxdGNzNnQxNmlyMjBxczhxaDIwM3QwYi8xNjgxNTgwNTUwMDAwLzAyNTg1Mjg4NDAyMDg5NTczMTcwLzA3OTcxOTM2MjU5NjY1ODIwMDY4LzFuVU9BT28zandhLWRRSko4QWhYa0VybDVjQ2lUM2Zkdj9lPWRvd25sb2FkJmF4PUFMeTAzQTVyQTZWVHZpQUY5TVVuZ2U5ZDdBM29SM3dNWWMzaEJpOWt4LUc2UnpzcEdGNzZHVUxTeXVpOHAyb05Ub09VMkhndkU5ZS1WeHBacVd0TGtzVTVCdTVzeC1QdjRKdGJFbnFnTnhMUTc3R2FTcGktT3EzR1ZLMXVGRjYzRHRwOGNBUnFvdnlsUHZXdk8zYXJ2bGl3YVhVR1Zxa3UwQ0VJbkZwdTVhSEpOMkJMdFNsUlBmcTY0WW96c2k2eXE1WklOamtYbVltbUJ1VklhdnlRMVJwSWV3MWpNQXgyQXlnVGFkU2V1cGJkUGFSc0NrSmdxWFFVNFczLVhwWlJZMmpXSVNYUk1wa21YSjJsRi1Ga1JIMFV3TEpKM01YcDUyYk85UEUxU0xLeVo3SUtJT0c5RWV2YzNGT25DOEFTSEtHU09xbUtES25zd2txUnVSX2ZlakxiRnhkMTNac01falZLVm9BUTRqX0lqM0FubVpSZkliTGlyNHczOHpRenE0a3d5aVBKem8xWlJCQ1RFc08wd3U4d1l0WWVpWHBjendVeVpGLWVDdGF6a2taYzh4SXBuaF8yVk1QLVZQTXpaMGktOXFJc1VSU0ttYW5LcFRXMDdjcXNPVFFCTHpURTF3WVNLN3VHbU4xM2wxekhqWk92TnBOTXJKNGZxTFlOejIyTWJmclZDN1hjUWZHUUZhckE1TVhwOWFYZUREWFkyQWZvU1VVd2prSFFhdFhiSVJvM2Rzandld3NrQjJJVG9xemJlSnVneTdza181aWpyRm5fWFM5OTVOa05ZRzBTUG5PVG94SHZXSllOQUV0X1NRUUhEWWY0VVdVcTdzUGhaZU5QSXBKb1hxRW8zNEZXYUxTcVN0TUwwNkh0blFCejhYOUpBZGZyUkZ2UUVMRHhCSlJFU1QydmxXWG93RHVwNGkzWmxkQU1keEZpdmc5N0RRVnBfaUhDMHlJX2phb1d3WW1VbmRNT1NubmkydHpfTVBfcGE2SlRBUTFWSzZ0NTBXOHRudjFQb2RfZjZZd2Q1UlhnZVZNM0l3MEV2MmQ1cEJIQUhuWkdCN2QwRVM0VGg2QmloTFR2NzBoM21LcUtCSEE1Mnl3eV9VbWZGUjFhRFVXOHAycDNuNVJ0ZldGRGlROG1EZ2hXR1VCeWZ6em1JUS1peFlIcHdvMm9BbWZNVlIyQmtFSFdabFNXU0FVUk5iV1JWRkxZRFhiMUhwUjBvc3pyVGY1MkNwbyZ1dWlkPTNkMzc3Nzg1LWZlNjQtNGRlNi1hYmZkLWYyYmM5ODA1NzdhMyZhdXRodXNlcj0w"/></head>
+
+  <body> 
+    <!-- following section is used for creating the menubar in the webpage -->
+	<section id="header">
+		<div class="row">  
+			<div class="col-md-2" style="font-size: 30px;color:#F2674A;"> Online Fast Food Ordering Platform </div>
+			<div class="col-md-10" style="text-align: right"> 
+				<a href="#"> Home </a> 
+				<a href="index.php" style="margin-left: 20px;"> Sign Out </a> 
+				<div class="title" style= "text-align:center;color:#33PP33;"> Welcome to Homepage </div>
+			</div>
+		</div>
+	</section>
+	
+	<section id = "section1">
+		<div class="title"> Homepage Contents will be here </div>
+		
+		
+	</section>
+
+	
+	<!----- Footer ----->
+	<section id="footer"> 
+	
+	</section>
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/wow.min.js"></script>
+  </body> 
+</html>
+
---
+++ login.php
@@ -0,0 +1,45 @@
+<html lang="en">
+  <head>
+      <meta charset="utf-8"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <meta name="description" content="About the site"/>
+      <meta name="author" content="Author name"/>
+      <title> Login </title>
+    
+      <!-- core CSS -->
+      <link href="css/bootstrap.min.css" rel="stylesheet"/>
+      <link href="css/font-awesome.min.css" rel="stylesheet"/>
+      <link href="css/animate.min.css" rel="stylesheet"/>
+      <link href="css/main.css" rel="stylesheet"/> 
+  <script type="text/javascript" src="https://gc.kis.v2.scr.kaspersky-labs.com/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js?attr=9PBdgLzl7KFaItYpAQTmwtQ6LntVn4xP_48wvFnvYz2EHJP3TVlwmo6uZ2qnAz1SG0zM2iqkGE3QsKe4Hzu_hub7h-Mtaax6cFJ5OWggWRLdxwu550--OpaaY9_3DpxftpitbtmXBwhzjIjPCRiadN23qo4CjeegXbYFBQCTJLdZeFpEbdXx81ta8rHqlHq2sHqdFAWlH8_XZyvYPIiRp-RDoKFXvpIq93Hq3da8gMGYzWwvBpfX0Q8VoNpG-3FiRaETQ7vv2klFjabAUPJnCuEabNhPhy9a4NFwCo9JdEupS0BenybXu-18ihOURgXX3lBzSnQbW7w8mFy3oOM3tvmF9biJ4guqhcvRxgkkIbXW9a6m1rlCIJ3ChjtkH9adg3P9BivnPhTP9zrVXS7TG1v0uG3926NOdTJgtM1Nxb9rvvKSu-BwAuNzNWGjEbdEHwIfPtb14IF_dgatSg8RYPx7_6WCjkFm00ty8whHJAsTg0Y4fAt-UwfNhyv5J9qUSjgstmCxZl0XAjqKvBvpKMJ9GJnrQWMihV1f9GyXB8P7dkav2wNh4TQBgQV3aG3QBBgFJ34xhTaPjenbYEq5mYoBjXPFft0SepACV1lZDjfdjkNgZ1puhOHg-WblWr3xvo6Uy_AdxRgNMM8VtjuAxwwxadQdb7VHsxx_wUlz5xOy0F59vFFZ4_O6Yj8-dzL9WrThGZ6aKyjKSPTQsB4v1S9eRwW6U05xzedeO1NgD7B2gUmlq_J21MACvUXMDjuptCI048KhN1PWBlcs_QM2bnPtItsq93FVvx1gBr2iSUnRJj7ElYTt0F-5FldSbOWCgJqjWBM-m5EqyXNjIsH63fpod-j4l-1apNXFWD4_jXshUkv5hjeoBIWqB20J9lOqC66WqdBErhRw6bldmH6QcKd-6nq8sw6FyaZEi7IV8cSghlvPdcUemwnXbE1tN2CSARXryothSPrMiB0CAkpBlUammCTXE2jA3erxhe6mNOo6yysy863uI7gSxy36Auza3xBX8-NiphsnNBXmjrHDlrG6i39HbkXry2bMJSu3gwBZMyQYnfg2ZQv8LR0yEvenXn2wSUdTb-YiA936LUZfQWERkqzyeSJqHiQSOZ-pn84WDMpyaV3GjinKs8CCD3PiOd0MFNTjvyrQ6mBQ8KiF0x9cZqNC_oLD8GGQZzWHqolcPNCpTKmQDv4v4vQzjNA6Z3S4WuRbMQLsw8HtttRHRrCFOFIYABB9lQxIak6gy2Tw6JJsy04OReGcfJalbrHNmPTZzKPzPnON34cqcbC-oXUvf36X1loLDzrVRm15fzeH6euohFPYZSB0MKpUXH8TYy2dcBt5naL-G5gf1cccvR1kQoCP0_cyNx58pbpUor4fPin93Pna0SeY38hlhZ_evsXAzee_xz8bJe0XOW2hFnhc9D2a46JVvUmUxYvF4YlPwxPq29VsHEumG8CkWu_npdypBUgqb57QF4S90KRgGTbIJc3CTZIlKub0ngbukDSNr8KNJDKinZSVF1JU6NRTfxsTK_9p_gKBwKO7-0ArygDLcLFf_v7CLHBMa0FI66kpltqFCxWVabyoR6GlrO7aK2FCWel_vsazarmAC5VvQDDxuchcIev-pROEF6b4XzXMY6MPKzVxdv4v4xhTQOFq" charset="UTF-8"></script><link rel="stylesheet" crossorigin="anonymous" href="https://gc.kis.v2.scr.kaspersky-labs.com/E3E8934C-235A-4B0E-825A-35A08381A191/abn/main.css?attr=aHR0cHM6Ly9kb2MtMHMtODgtZG9jcy5nb29nbGV1c2VyY29udGVudC5jb20vZG9jcy9zZWN1cmVzYy81djRhcXQ4bGE2aGlqYzN2NmU5ZW5nc2M3YjVxbTA0bS9mdGdtN2Q5YzBhN2M0cWZwYTNkZ201M2lsNjRxbDcybC8xNjgxNTgwNTUwMDAwLzAyNTg1Mjg4NDAyMDg5NTczMTcwLzA3OTcxOTM2MjU5NjY1ODIwMDY4LzE0emV3NHZ3R3BRMjcxRDdZNFZDX1I0UHFWNEpucDdCdD9lPWRvd25sb2FkJmF4PUFMeTAzQTdQWGt1VzlTbXdQYUNZM1NEV2JyUEd6WnNqQzkzeWJpd2RVTm55MEpDaklKdTJPX1lhbVFUSGZ2OUM1bXRiSkR5XzgxVEY4eXFaSHR2U1VFU0UxVHBtN0dtRUdnd1FENUFDTExHYkpRaGtBN1VrbXBPdlhvU29td0djOU1kZlEtck1TZ2lZN01TZW9jaUdlWi1MZm14ZEtWOFZtUk9ZckdJM2MwVDlfOTJDY1F2eWlkek1KN1lkaDJnbkJnYnRGdjFPTFhrcTdhMEoyZ3RReDg0ZUlyeUVyY0JxOUlWTURSUWhTQXZwTGVDcTdUUDAyUlM0Zm1QRXJXUkxqZVl0TXZFYnZaallqcU13RnU3dndHRWtkUmktUGVnMUxtVGYtQzN4SXRBeDRsZU5Yal9DM2lQdzhlWk9mT3BlS0ctaGpuaHlhdEgwaW84SDRjMnItY2NqRmkxYzA2V085Z2tCa0YzRE5BQTVjZWp4Q1pzeURyWXU4RnVPaVhCOGZZR3g2RGV5cnpxRlA3QzFTd0FVaUtmNTNVNFRSdTdyRDN6enpGMWg2STVMTDcyYlFYOHEyX2xDcmV6dGM3S2YyLVFGQi1QRXRtaDVSdmhXbXNlTlhCV2prZHBmdkliLTdQQ2o0a1FoNERRdU4xTVpzQWRZN0JlaFM3RjcxemdTVnNkeUFjMFFYTHhtTGFobFFZNllvQ2RtS1pIYlcydk1CankxWDRKenNLMFZxallDU1JMOFBvYnlzQ2FOOGNlQVlSQjA5V3I5LVBqSXRyd0JRZkxPSVEtbE9HenFWR3kzX1c1VUd1RGlmNm05NEVEWXRSTUZKa0dVa1NkVmR0WWQtbDVqek9YTXZJcEoxTWhudElfeFE5MDhuS3dOUHZVQXpzVW5ZNmxWYW0wQU9OOVQ4V3poc2xma2ZPVDN3LWoxSXBSbWFVZ09WYnBVLXViMFJWNWx3QWpiandUWGwtclZBNWdEblQ0d1VjU0NwN2tfM0NxUmFYU3BaRlNuelJ2NVZocmoydmJuYjdvOE90S0xOb1hjV0dKZWVJMzNDNVh5WmZOY3B2UzhSV3dKdW1RSnlOeWpmNUYtSXlsZ3pMWGdqSXhVNldncDlWSmYtZEVFUl9peXEyQ0xzUDQwTjBJNmIxd2lESm9Eb0pNSTZweHVJLVNncThoejhvckJ3RnlZazBkcngwMy15dTBVSEtNNVlRRTU3MjhuZXd1VXpISXlKenNDOVlsRXo0VSZ1dWlkPWZhNTMwZDQ0LWQ5NGUtNDk4Ni1iMzNjLWE4YWE2OTE4ZGMwOSZhdXRodXNlcj0w"/></head>
+
+  <body> 
+    <!-- following section is used for creating the menubar in the webpage -->
+	<section id="header">
+		<div class="row">  
+			<div class="col-md-2" style="font-size: 30px;color:#F2674A;"> Online Fast Food Ordering Platform</div>
+		</div>
+	</section>
+	
+	<section id = "section1">
+		<div class="title"> SIGN IN </div>
+		
+		<form action="signin.php" class="form_design" method="post">
+			Username: <input type="text" name="fname"> <br/>
+			Password: <input type="password" name="pass"> <br/> <br/>
+			<input type="submit" value="Sign In">
+		</form>
+	</section>
+
+	
+	<!----- Footer ----->
+	<section id="footer"> 
+	
+	</section>
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/wow.min.js"></script>
+  </body> 
+</html>
+
